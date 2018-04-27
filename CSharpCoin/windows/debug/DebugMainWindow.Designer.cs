2256be6d3951f650fa7cbbb004c67fb77c4b2b61
Daniel <daniel.viduedo@hotmail.com>
2018-04-27 19:37:04 -0300

    Primeiro commit

---
+++ CSharpCoin/windows/debug/DebugMainWindow.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CSharpCoin.windows.debug
+{
+    partial class DebugMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtInput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtInput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 418);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(776, 26);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtOutput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtOutput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtOutput.Location = new System.Drawing.Point(12, 12);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(776, 400);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.Text = "";
+            // 
+            // DebugMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.MaximizeBox = false;
+            this.Name = "DebugMainWindow";
+            this.Text = "CSharpCoin - Debug";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.RichTextBox txtOutput;
+    }
+}
---
+++ CSharpCoin/windows/debug/DebugMainWindow.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpCoin.windows.debug
+{
+    public partial class DebugMainWindow : Form
+    {
+
+        private String data;
+        private String commandText;
+
+        public DebugMainWindow()
+        {
+            InitializeComponent();
+
+            InsertHeader();
+        }
+
+        // Caso seja pressionado o Enter, o programa dispara a ação...
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                InsertDataFromInput();
+                VerifyCommand(data);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        // Insere o Cabeçalho Inicial no RichTextBox (txtOutput)
+        private void InsertHeader()
+        {
+            String[] title = { "             --------------- CSharpCoin - Debug Console --------------- \n"};
+
+            for (int i = 0; i < title.Length; i++)
+            {
+                txtOutput.AppendText(title[i]);
+            }
+        }
+
+        // Insere dados do TextBox (txtInput) no RichTextBox (txtOutput)
+        private void InsertDataFromInput()
+        {
+            data = txtInput.Text; // Insere os dados do TextBox na variável GLOBAL
+            txtOutput.AppendText(" " + data + "\n");
+            txtInput.Text = ""; // Zera o TextBox
+        }
+
+        private void VerifyCommand(String data)
+        {
+            if (data.Equals("help"))
+            {
+                commandText = " test \n";
+                txtOutput.AppendText(commandText);
+            }
+
+            if (data.Equals("clear"))
+            {
+                txtOutput.Clear();
+            }
+
+        }
+
+    }
+}
